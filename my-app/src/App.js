4013a787bdc8cb44bb719bbdcda6f83d5f36db6f
apln-n <applingo@eis.hokudai.ac.jp>
2022-03-21 20:59:00 +0900

    Create React App のテンプレート作成

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+//return()のdivの中に具体的なアプリケーションを書いていく
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React!!!</h1>
+      <p>This is apln-n's personal application.</p>
+    </div>
+  );
+}
+ 
+export default App;
