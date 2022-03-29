2e54aa899f894e7f0212515cc0338c3dd3babf51
apln-n <applingo@eis.hokudai.ac.jp>
2022-03-29 18:27:18 +0900

    tutorial.jsをcreate-react-appとreact-routerを使う場合の物に書き替えた(classをfunctionにしたり)

--- my-app/src/myfiles/tutorial.js
+++ my-app/src/myfiles/tutorial.js
@@ -1,70 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/*
+  公式チュートリアル(三目並べ)
+  https://ja.reactjs.org/tutorial/tutorial.html#setup-option-2-local-development-environment
+*/
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+function Square() {
+  return (
+    <button className="square">
+      {/* TODO */}
+    </button>
+  );
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
+function Board() {
+  function renderSquare(i) {
     return <Square />;
   }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  const status = 'Next player: X';
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
       </div>
-    );
-  }
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+      <div>
+        <p><Link to="/">ホームに戻る</Link></p>
+      </div>
+    </div>
+  );
 }
 
 // ========================================
 
 function func(){
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
+  return <Game />
 }
 
 export default func;
