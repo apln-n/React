c97bcca6136b239c08d0be5393281ce1232c9421
apln-n <applingo@eis.hokudai.ac.jp>
2022-03-30 19:08:03 +0900

    classを使わずにclassっぽいことを実現(チュートリアルの準備まで

--- my-app/src/myfiles/tutorial.js
+++ my-app/src/myfiles/tutorial.js
@@ -1,6 +1,9 @@
 /*
   公式チュートリアル(三目並べ)
-  https://ja.reactjs.org/tutorial/tutorial.html#setup-option-2-local-development-environment
+    https://ja.reactjs.org/tutorial/tutorial.html#setup-option-2-local-development-environment
+  ES6以前のクラス定義の実現(これで作りたい)
+    https://www.yunabe.jp/docs/javascript_class_in_google.html
+    this.を使うことでクラスの属性みたいに扱えるようになる
 */
 
 import React from 'react';
@@ -8,73 +11,60 @@ import ReactDOM from 'react-dom';
 import { Link } from "react-router-dom";
 
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+let Square = function(i) {
+  this.button = (
+    <button className="square">
+      {i}
+    </button>
+  );
+};
+
+let Board = function() {
+  const status = 'Next player: X';
+
+  this.Squares = (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {new Square(0).button}
+        {new Square(1).button}
+        {new Square(2).button}
+      </div>
+      <div className="board-row">
+        {new Square(3).button}
+        {new Square(4).button}
+        {new Square(5).button}
+      </div>
+      <div className="board-row">
+        {new Square(6).button}
+        {new Square(7).button}
+        {new Square(8).button}
+      </div>
+    </div>
+  );
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+let Game = function() {
+  this.board = (
+    <div className="game">
+      <div className="game-board">
+        {new Board().Squares}
       </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-        <div className="back-to-top">
-          <p><Link to="/">ホームに戻る</Link></p>
-        </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
       </div>
-    );
-  }
+      <div className="back-to-top">
+        <p><Link to="/">ホームに戻る</Link></p>
+      </div>
+    </div>
+  );
 }
 
 // ========================================
-
 function func(){
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
+  var game = new Game();
+  return game.board;
 }
 
 export default func;
